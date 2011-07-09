cefb5b71beb5a3e7327235634827ccb2bd188f74
kmshi <kmshi_array@yahoo.com>
2011-07-09 19:33:26 +0800

    revert back to naked ip to avoid GFW domain hijack

--- model.py
+++ model.py
@@ -434,7 +434,8 @@ class Entry(BaseModel):
 
 	@property
 	def miro_subscription_url(self):
-                miro_base_url = 'http://subscribe.idearadar.org/p/get?'
+                #miro_base_url = 'http://subscribe.idearadar.org/p/get?'
+                miro_base_url = 'http://crowdwisdom.co.cc/p/get?'
                 if 'videocast' in [cat.slug for cat in self.categories] and self.sourcefeed:
                         params={'type':'video','url1':self.sourcefeed,'trackback1':self.fullurl}
                         return cgi.escape(miro_base_url + urllib.urlencode(params))
